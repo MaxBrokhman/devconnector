3741d21db89389658a3f7773f73343ca6390a0e2
MaxBrohman <maksimbrochman@gmail.com>
2020-05-06 22:08:53 +0300

    saving user to collection implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const { check, validationResult } = require('express-validator')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+const {UserModel} = require('../../models/User')
 
 const userRouter = express.Router()
 
@@ -7,14 +11,53 @@ userRouter.post('/', [
   check('name', 'Name is required').notEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({
       errors: errors.array(),
     })
   }
-  res.send('User route')
+
+  const {
+    name,
+    email,
+    password,
+  } = req.body
+
+  try {
+    const user = await UserModel.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({
+        errors: [{
+          message: 'User with this email already exists',
+        }]
+      })
+    }
+
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm',
+    })
+
+    const salt = await bcrypt.genSalt(12)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    
+    const newUser = await new UserModel({
+      name,
+      email,
+      avatar,
+      password: hashedPassword,
+    }).save()
+
+    res.send('User registered')
+
+  } catch {
+    res.status(500).send('Server error')
+  }
+  
 })
 
 module.exports = {
