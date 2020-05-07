905290085f460a8274f8ce7e2721d5d4982fa253
MaxBrohman <maksimbrochman@gmail.com>
2020-05-07 12:52:10 +0300

    login path added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const {UserModel} = require('../../models/User')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 
+const {UserModel} = require('../../models/User')
 const { makeAuth } = require('../../middleware/auth')
 
 const authRouter = express.Router()
 
+// Sign up
 authRouter.get('/', makeAuth, async (req, res) => {
   try {
     const user = await UserModel.findById(req.user.id).select('-password')
@@ -14,6 +19,59 @@ authRouter.get('/', makeAuth, async (req, res) => {
       message: 'User not found',
     })
   }
+})
+
+// Log in
+authRouter.post('/', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists(),
+], 
+async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    })
+  }
+  const { email, password } = req.body
+  try {
+    const user = await UserModel.findOne({ email })
+    if (!user) {
+      return res.status(400).json({
+        errors: [{
+          message: 'Invalid credentials.',
+        }]
+      })
+    }
+    const isPasswordRight = await bcrypt.compare(password, user.password)
+    if (!isPasswordRight) {
+      return res.status(400).json({
+        errors: [{
+          message: 'Invalid credentials.',
+        }]
+      })
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    }
+    jwt.sign(
+      payload, 
+      config.get('jwtSecret'), 
+      {
+        expiresIn: 360000,
+      },
+      (error, token) => {
+        if (error) throw error
+        res.status(200).json({ token })
+      }
+    )
+  } catch {
+    res.status(500).send('Server error')
+  }
+  
 })
 
 module.exports = {
