13be199984e5fe43c6f32fa25f5a580387a6d386
MaxBrohman <maksimbrochman@gmail.com>
2020-05-07 13:23:16 +0300

    profile/me route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express')
 
+const { makeAuth } = require('../../middleware/auth')
+const { ProfileModel } = require('../../models/Profile')
+
 const profileRouter = express.Router()
 
-profileRouter.get('/', (req, res) => {
-  res.send('Profile route')
+profileRouter.get('/me', makeAuth, async (req, res) => {
+  try {
+    const profile = await ProfileModel
+      .findOne({ user: req.user.id })
+      .populate('User', ['name', 'avatar'])
+
+    if (!profile) {
+      return res.status(400).json({
+        message: 'There is no profile for this user',
+      })
+    }
+
+    res.json({ profile })
+  } catch {
+    res.status(500).send('Server error')
+  }
 })
 
 module.exports = {
