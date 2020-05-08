3bfd513db47b6d085b0e59328f290a15ff4944e0
MaxBrohman <maksimbrochman@gmail.com>
2020-05-08 15:59:46 +0300

    add new post route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator')
+
+const { PostModel } = require('../../models/Post')
+const { UserModel } = require('../../models/User')
+const { makeAuth } = require('../../middleware/auth')
 
 const postRouter = express.Router()
 
-postRouter.get('/', (req, res) => {
-  res.send('Post route')
+postRouter.post('/', [
+  makeAuth,
+  check('text', 'Text is required').notEmpty(),
+], 
+async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    })
+  }
+
+  try {
+    const user = await UserModel.findById(req.user.id).select('-password')
+    const newPost = await new PostModel({
+      ...req.body,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    }).save()
+
+    res.json({ post: newPost })
+  } catch {
+    res.status(500).send('Server error')
+  }
 })
 
 module.exports = {
